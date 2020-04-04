a955b698f436d2910b6b8702600d4300d30f0970
aloptrbl <thatzelo@gmail.com>
2020-04-05 02:12:20 +0800

    Update Sign in & Landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,28 +2,65 @@ import React from 'react';
 import Navigation from '../Navigation';
 import Bus from '../../img/bus.svg';
 const LandingPage = () => (
-  <div class="flex flex-col lg:flex-row flex-shrink-0 bg-white-900 text-white h-screen overflow-hidden">
-  <div class="flex-1 overflow-y-auto">
-      <div class="container mx-auto px-8 py-auto">
-          <main class="flex flex-col-reverse sm:flex-row jusitfy-between items-center py-12">
-              <div class="sm:w-2/5 flex flex-col items-center sm:items-start text-center sm:text-left">
-                <div class="flex-row flex">
-                <div class="flex-col flex">
-                <img src={Bus} class="w-48 pl-10" />
-                <h1 class="uppercase text-6xl text-blue-900 font-bold leading-none tracking-wide mb-2">UTeM Bus Tracking</h1>
-                  <h2 class="uppercase text-4xl text-orange-500 text-secondary tracking-widest mb-6">WEB APP</h2>
-                  </div>
-   
-                </div>
-                 
-                  <a href="#" class="bg-purple-300 hover:bg-purple-400 py-3 px-6 uppercase text-lg font-bold text-white rounded-full">Sign In</a>
-              </div>
-              <div class="mb-16 sm:mb-0 mt-8 sm:mt-0 sm:w-3/5 sm:pl-12">
-   
-              </div>
-          </main>
-      </div>
+<div class="">
+<nav class="flex items-center justify-between flex-wrap bg-blue-800 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+  <svg version="1.1" id="Layer_1" fill="white" height="50" width="50" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+	 viewBox="0 0 100 100">
+<path d="M89,38.5c3.3,0,6-2.7,6-6v-10c0-3.3-2.7-6-6-6h-4v-4c0-3.3-2.7-6-6-6H21c-3.3,0-6,2.7-6,6v4h-4c-3.3,0-6,2.7-6,6v10
+	c0,3.3,2.7,6,6,6h4v6h-4c-3.3,0-6,2.7-6,6v27c0,3.3,2.7,6,6,6v4c0,3.3,2.7,6,6,6h8c3.3,0,6-2.7,6-6v-4h38v4c0,3.3,2.7,6,6,6h8
+	c3.3,0,6-2.7,6-6v-4c3.3,0,6-2.7,6-6v-27c0-3.3-2.7-6-6-6h-4v-6H89z M85,20.5h4c1.1,0,2,0.9,2,2v10c0,1.1-0.9,2-2,2h-4V20.5z
+	 M63,10.5h16c1.1,0,2,0.9,2,2v4H63V10.5z M41,10.5h18v6H41V10.5z M19,12.5c0-1.1,0.9-2,2-2h16v6H19V12.5z M19,20.5h62v24H67.9
+	c-0.5-2.8-2.9-5-5.9-5H38c-3,0-5.4,2.2-5.9,5H19V20.5z M36,59.5v-6h28v6H36z M64,63.5v6H36v-6H64z M36,49.5v-4c0-1.1,0.9-2,2-2h24
+	c1.1,0,2,0.9,2,2v4H36z M11,34.5c-1.1,0-2-0.9-2-2v-10c0-1.1,0.9-2,2-2h4v14H11z M9,50.5c0-1.1,0.9-2,2-2h21v21h-6v-10
+	c0-2.8-2.2-5-5-5H9V50.5z M9,58.5h12c0.5,0,1,0.5,1,1v10H9V58.5z M27,87.5c0,1.1-0.9,2-2,2h-8c-1.1,0-2-0.9-2-2v-4h12V87.5z
+	 M85,87.5c0,1.1-0.9,2-2,2h-8c-1.1,0-2-0.9-2-2v-4h12V87.5z M91,77.5c0,1.1-0.9,2-2,2H11c-1.1,0-2-0.9-2-2v-4h82V77.5z M91,69.5H78
+	v-10c0-0.5,0.5-1,1-1h12V69.5z M89,48.5c1.1,0,2,0.9,2,2v4H79c-2.8,0-5,2.2-5,5v10h-6v-21H89z"/>
+</svg>
+    <span class="font-semibold text-xl tracking-tight">UTeM Bus Tracking Web App</span>
   </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+     
+      </a>
+
+    </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign In</a>
+    </div>
+  </div>
+</nav>
+<div class="lg:container mx-auto">
+<div class="flex flex-col lg:flex-row flex-shrink-0 bg-white-900 text-white h-screen overflow-hidden">
+<div class="flex-1 overflow-y-auto">
+    <div class="container mx-auto px-8 py-auto">
+        <main class="flex flex-col-reverse sm:flex-row jusitfy-between items-center py-12">
+            <div class="sm:w-2/5 flex flex-col items-center sm:items-start text-center sm:text-left">
+              <div class="flex-row flex">
+              <div class="flex-col flex">
+              <img src={Bus} class="w-48 pl-10" />
+              <h1 class="uppercase text-6xl text-blue-900 font-bold leading-none tracking-wide mb-2">UTeM Bus Tracking</h1>
+                <h2 class="uppercase text-4xl text-orange-500 text-secondary tracking-widest mb-6">WEB APP</h2>
+                </div>
+ 
+              </div>
+               
+                <a href="/signin" class="bg-blue-800 hover:bg-blue-400 py-3 px-6 uppercase text-lg font-bold text-white rounded-full">Sign In</a>
+            </div>
+            <div class="mb-16 sm:mb-0 mt-8 sm:mt-0 sm:w-3/5 sm:pl-12">
+ 
+            </div>
+        </main>
+    </div>
+</div>
+</div>
+</div>
 </div>
 );
 
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -5,9 +5,9 @@ import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 import { PasswordForgetLink } from '../PasswordForget';
+import Bus from '../../img/realbus.jpg';
 const SignInPage = () => (
   <div>
-    <h1>SignIn</h1>
     <SignInForm />
     <PasswordForgetLink />
     <SignUpLink />
@@ -40,29 +40,70 @@ class SignInFormBase extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
   render() {
+    const Background = 'https://images.unsplash.com/photo-1515965885361-f1e0095517ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80';
+    const mystyle = {
+      background:`url(${Bus})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+    };
     const { email, password, error } = this.state;
     const isInvalid = password === '' || email === '';
     return (
       <form onSubmit={this.onSubmit}>
-        <input
+      <div class="bg-blue-400 h-screen w-screen">
+      <div class="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
+          <div class="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 bg-white sm:mx-0" style={{height: 500}}>
+              <div class="flex flex-col w-full md:w-1/2 p-4">
+                  <div class="flex flex-col flex-1 justify-center mb-8">
+                      <h1 class="text-4xl text-center font-thin">Sign In</h1>
+                      <div class="w-full mt-4">
+                              <div class="flex flex-col mt-4">
+                              <input
+                            id="email"
+                            class="flex-grow h-8 px-2 border rounded border-grey-400" 
           name="email"
           value={email}
           onChange={this.onChange}
           type="text"
           placeholder="Email Address"
         />
-        <input
+                              </div>
+                              <div class="flex flex-col mt-4">
+                              <input
+                              id="password"
           name="password"
           value={password}
+          class="flex-grow h-8 px-2 rounded border border-grey-400"
           onChange={this.onChange}
           type="password"
           placeholder="Password"
         />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
+                              </div>
+                              <div class="flex items-center mt-4">
+                                  <input type="checkbox" name="remember" id="remember" class="mr-2" /> <label for="remember" class="text-sm text-grey-dark">Remember Me</label>
+                              </div>
+                              <div class="flex items-center mt-4">
+                              {error && <p>{error.message}</p>}
+                              </div>
+                              <div class="flex flex-col mt-8">
+                                  <button type="submit" disabled={isInvalid}  class="bg-blue-700 disabled:opacity-25 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
+                                      Sign In
+                                  </button>
+                              </div>
+                             
+                          <div class="text-center mt-4">
+                              <a class="no-underline hover:underline text-blue-dark text-xs" href="{{ route('password.request') }}">
+                                  Forgot Your Password?
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="hidden md:block md:w-1/2 rounded-r-lg" style={mystyle}></div>
+          </div>
+      </div>
+  </div>
+      </form>  
     );
   }
 }
